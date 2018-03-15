686be76fe7db7270df10bf0a6192d478937c0126
Phoebe Lau <phoebelau7@gmail.com>
2018-03-15 04:59:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ function driversWithRevenueOver(drivers, revenue){
 }
 
 function driverNamesWithRevenueOver(drivers,revenue){
+  const newCollection = [];
   return drivers.filter(function(driver) {
     return driver.revenue > revenue
-    return driver.name
+    newCollection.push(driver.name)
   });
 }
