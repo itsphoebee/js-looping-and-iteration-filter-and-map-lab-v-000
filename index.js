36ced025d5387c9b634c27f2d6f804023822b089
Phoebe Lau <phoebelau7@gmail.com>
2018-03-15 05:15:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ function exactMatch(drivers, attribute){
     return newCollection;
   });
 }
+
+function exactMatchToList(drivers, attribute) {
+  return exactMatch(drivers, attribute).map(function(driver){
+    return driver.name
+  });
+}
