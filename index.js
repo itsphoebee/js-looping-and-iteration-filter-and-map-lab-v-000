4c3f10d3df2b61dadb1741a072abc92e52a41442
Phoebe Lau <phoebelau7@gmail.com>
2018-03-15 04:49:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
-function driversWithRevenueOver(array, revenue){
-  array.filter(function(driver) {
+function driversWithRevenueOver(drivers, revenue){
+  return drivers.filter(function(driver) {
     return driver.revenue > revenue
   });
 }
